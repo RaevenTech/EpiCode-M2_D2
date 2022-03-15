9a7d81d805650de0cd0493937f5d2b751c095c9f
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-03-15 14:18:17 +0000

    javaScript EX

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 /*
 MANDATORY JS EXERCISES (create a different file for this exercise)
-
+    1) Save this array in a variable called genres: ["Comedy", "Drama", "Romance", "Horror", "Sci-Fi", "Documentary"]
     2) Create an unordered list using JavaScript and save it in a variable
     3) Cycle the array and create a list-item via JavaScript for every element
     4) Insert the genere as text in the list-item
     5) Append the list items in the unordered list
     6) Append the unordered list in the document. The list should appear in the page.
 */
+let genres = ["Comedy", "Drama", "Romance", "Horror", "Sci-Fi", "Documentary"]
 
-// 1) Save this array in a variable called genres: ["Comedy", "Drama", "Romance", "Horror", "Sci-Fi", "Documentary"]
+function myFunction() {
+    let ul = document.createElement("ul")
+
+    for (let i = 0; i < genres.length; i++) {
+        let li = document.createElement("li")
+        li.innerText = genres[i]
+        ul.appendChild(li)
+        document.body.appendChild(ul)
+    }
+}
